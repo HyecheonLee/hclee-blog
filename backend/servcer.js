c2e292ed667489a2e5eeb1cba7e3664df8993045
hyecheon <rainbow880616@gmail.com>
2020-06-03 23:55:25 +0900

    Initial commit

---
+++ backend/servcer.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const mongoose = require('mongoose');
+const cors = require('cors')
+require("dotenv").config()
+//bring routes
+const blogRoutes = require('./routes/blog')
+const authRoutes = require('./routes/auth')
+
+// app
+const app = express()
+
+//db
+mongoose.connect(process.env.DATABASE_LOCAL,
+    {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true
+    })
+.then(() => console.log('DB connected'))
+.catch(err => {
+  console.log(err);
+});
+// middleware
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+app.use(cookieParser())
+
+//cors
+if (process.env.NODE_ENV === 'development') {
+  app.use(cors({origin: `${process.env.CLIENT_URL}`}))
+}
+
+//routes middleware
+app.use('/api', blogRoutes)
+app.use('/api', authRoutes)
+
+const port = process.env.PORT || 8000
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+})
