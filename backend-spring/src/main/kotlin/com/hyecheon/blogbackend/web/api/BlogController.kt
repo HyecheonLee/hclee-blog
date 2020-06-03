d5d42ba7883d211a10f3970416603453f8b8e1f0
hyecheon <rainbow880616@gmail.com>
2020-06-04 02:40:34 +0900

    error 처리.?

--- backend-spring/src/main/kotlin/com/hyecheon/blogbackend/web/api/BlogController.kt
+++ backend-spring/src/main/kotlin/com/hyecheon/blogbackend/web/api/BlogController.kt
@@ -4,6 +4,6 @@ import org.springframework.web.bind.annotation.RestController
 import java.time.LocalDateTime
 
 @RestController("/api/blog")
-class Blog {
+class BlogController {
 	fun index() = LocalDateTime.now()
 }
