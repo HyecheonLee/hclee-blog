fdd62eb09d53e0bbfba41a5188f9dc086fd94bb0
hyecheon <rainbow880616@gmail.com>
2020-06-10 01:08:39 +0900

    validator 수정.

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,10 +3,10 @@ const router = express.Router()
 const {signup, signin, signout, requireSignin} = require("../controllers/auth")
 
 const {runValidation} = require('../validators')
-const {userSignupValidator} = require('../validators/auth')
+const {userSignupValidator, userSigninValidator} = require('../validators/auth')
 
 router.post('/signup', userSignupValidator, runValidation, signup);
-router.post("/signin", userSignupValidator, runValidation, signin);
+router.post("/signin", userSigninValidator, runValidation, signin);
 router.get("/signout", signout);
 //test
 router.get("/secret", requireSignin, (require, res) => {
