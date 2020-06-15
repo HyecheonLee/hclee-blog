8e25c12cb20b55b9a5eb3cea7fea7058846b3ba2
hyecheon <rainbow880616@gmail.com>
2020-06-16 00:41:10 +0900

    [backend] blog api

---
+++ backend/helpers/blog.js
@@ -0,0 +1,15 @@
+exports.smartTrim = (str, length, delim, appendix) => {
+  if (str.length <= length) {
+    return str;
+  }
+  let trimmedStr = str.substr(0, length + delim.length);
+
+  let lastDelimIndex = trimmedStr.lastIndexOf(delim);
+  if (lastDelimIndex >= 0) {
+    trimmedStr = trimmedStr.substring(0, lastDelimIndex);
+  }
+  if (trimmedStr) {
+    trimmedStr += appendix;
+  }
+  return trimmedStr;
+}
