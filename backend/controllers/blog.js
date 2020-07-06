cbc9b52cf09fca0c6a6af2cc2b4e45feaae683f6
hyecheon <rainbow880616@gmail.com>
2020-07-07 00:36:47 +0900

    [backend] blogs page

--- backend/controllers/blog.js
+++ backend/controllers/blog.js
@@ -8,6 +8,7 @@ const _ = require('lodash')
 const {errorHandler} = require('../helpers/dbErrorHandler')
 const fs = require('fs');
 const {smartTrim} = require('../helpers/blog')
+
 exports.create = (req, res) => {
   let form = new formidable.IncomingForm();
   form.keepExtensions = true;
@@ -114,13 +115,152 @@ exports.list = (req, res) => {
   })
 }
 exports.listAllBlogsCategoriesTags = (req, res) => {
+  const limit = req.body.limit ? parseInt(req.body.limit) : 10
+  const skip = req.body.skip ? parseInt(req.body.skip) : 0
 
-}
+  let blogs
+  let categories
+  let tags
+
+  Blog.find({})
+  .populate("tags", "_id name slug")
+  .populate("postedBy", "_id name username")
+  .sort({createdAt: -1})
+  .skip(skip)
+  .limit(limit)
+  .select("_id title slug excerpt categories tags postedBy createdAt updatedAt")
+  .exec((err, data) => {
+    if (err) {
+      return res.json({
+        error: errorHandler(err)
+      })
+    }
+    blogs = data // blogs
+    // get all categories
+    Category.find({}).exec((err, c) => {
+      if (err) {
+        return res.json({
+          error: errorHandler(err)
+        });
+      }
+      categories = c // categories
+      Tag.find({}).exec((err, t) => {
+        if (err) {
+          return res.json({
+            error: errorHandler(err)
+          });
+        }
+        tags = t; //tags
+        //return all blogs categories tags
+        res.json({blogs, categories, tags, size: blogs.length});
+      });
+    });
+  });
+};
+
 exports.read = (req, res) => {
+
+  const slug = req.params.slug.toLowerCase();
+  Blog.findOne({slug})
+  .populate('categories', '_id name slug')
+  .populate('tags', '_id name slug')
+  .populate('postedBy', '_id name username')
+  .select(
+      '_id title body slug mtitle mdesc categories tags postedBy createdAt updatedAt')
+  .exec((err, data) => {
+    if (err) {
+      return res.json({
+        error: errorHandler(err)
+      });
+    }
+    res.json(data)
+  })
+
 }
 exports.remove = (req, res) => {
-
+  const slug = req.params.slug.toLowerCase();
+  Blog.findOneAndRemove({slug}).exec((err, data) => {
+    if (err) {
+      return res.json({
+        error: errorHandler(err)
+      });
+    }
+    res.json({
+      message: 'Blog deleted successfully'
+    });
+  })
 }
 exports.update = (req, res) => {
+  const slug = req.params.slug.toLowerCase();
 
+  Blog.findOne({slug}).exec((err, oldBlog) => {
+    if (err) {
+      return res.status(400).json({
+        err: errorHandler(err)
+      })
+    }
+    let form = new formidable.IncomingForm();
+    form.keepExtensions = true;
+    form.parse(req, (err, fields, files) => {
+      if (err) {
+        return res.status(400).json({
+          error: 'Image could not upload'
+        });
+      }
+
+      let slugBeforeMerge = oldBlog.slug
+      oldBlog = _.merge(oldBlog, fields);
+      oldBlog.slug = slugBeforeMerge
+
+      const {body, categories, tags} = fields;
+
+      if (body) {
+        oldBlog.excerpt = smartTrim(body, 320, '', ' ...')
+        oldBlog.mdesc = stripHtml(body.substring(0, 160))
+      }
+
+      if (categories) {
+        oldBlog.categories = categories.split(',')
+      }
+
+      if (tags) {
+        oldBlog.tags = tags.split(",")
+      }
+
+      if (files.photo) {
+        if (files.photo.size > 1000000) {
+          return res.status(400).json({
+            error: "Image should be less then 1mb in size"
+          });
+        }
+        oldBlog.photo.data = fs.readFileSync(files.photo.path);
+        oldBlog.photo.contentType = files.photo.type
+      }
+
+      oldBlog.save((err, result) => {
+        if (err) {
+          return res.status(400).json({
+            error: errorHandler(err)
+          });
+        }
+        // result.photo = undefined
+        res.json(result);
+      });
+    });
+  });
+}
+
+exports.photo = (req, res) => {
+  const slug = req.params.slug.toLowerCase();
+  Blog.findOne({slug})
+  .select('photo')
+  .exec((err, blog) => {
+    if (err || !blog) {
+      return res.status(400).json({
+        err: errorHandler(err)
+      })
+    }
+    res.set('Content-Type', blog.photo.contentType)
+    return res.send(blog.photo.data)
+  });
 }
